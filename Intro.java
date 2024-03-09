fa496b9c2349c677cb416714c886d52a8c7dcfcf
mahskas-ramot <sakshamtomar2014@gmail.com>
2024-03-09 17:33:31 +0530

    --> intro

---
+++ Intro.java
@@ -0,0 +1,12 @@
+import java.util.Stack;
+class Main{
+    public static void main(String[] args){
+        Stack<Integer> stack = new Stack<Integer>();
+        stack.push(34);
+        stack.push(52);
+        stack.push(54);
+        stack.push(89);
+        System.out.println(stack.peek());
+
+    }
+}
